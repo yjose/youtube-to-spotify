0709082719a789875eb1f7afab1247e40d72e2aa
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2025-04-08 16:26:29 +0100

    feat: set the cookies as base64

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh -l
 
+echo ::group::Signing in to yt-dlp YouTube using cookies
+mkdir -p ~/.cache/yt-dlp/youtube
+echo "$COOKIES_FILE" | base64 -d > ~/.cache/yt-dlp/youtube/cookies.txt
+mkdir -p ~/.config/yt-dlp
+echo "--cookies ~/.cache/yt-dlp/youtube/cookies.txt" >> ~/.config/yt-dlp/config
+echo "Config file content:"
+cat ~/.config/yt-dlp/config
+echo ::endgroup::
+
 npm start --prefix /
